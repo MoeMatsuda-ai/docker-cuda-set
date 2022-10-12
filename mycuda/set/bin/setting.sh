60daa10ae1e347527cfece79887037c504c13c22
MoeMatsuda-ai <91708458+MoeMatsuda-ai@users.noreply.github.com>
2022-10-12 21:45:59 +0900

    Update setting.sh

--- mycuda/set/bin/setting.sh
+++ mycuda/set/bin/setting.sh
@@ -6,3 +6,11 @@ apt-get install -y curl python3 python3-distutils openssh-server
 curl https://bootstrap.pypa.io/get-pip.py -O get-pip.py
 python3 get-pip.py
 pip install -r /tmp/requirements.txt
+
+# timezone設定をスキップするための設定(for installing openssh-server)
+export DEBIAN_FRONTEND=noninteractive
+# settings ssh
+apt-get install -y openssh-server 
+export mkdir /var/run/sshd
+export ENV=NOTVISIBLE "in users profile"
+echo "export VISIBLE=now" >> /etc/profile
